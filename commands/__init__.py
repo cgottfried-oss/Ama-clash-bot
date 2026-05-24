57713d0ae3f6b2e96b4b7ee4aae93a3e9f9a289e
cgottfried-oss <c.gottfried@icloud.com>
2026-05-24 06:19:29 -0700

    Register phase 3 clan economy commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,6 @@
 from .economy_commands import register_economy_commands
 from .economy_game_commands import register_economy_game_commands
+from .economy_phase3_commands import register_economy_phase3_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
 
@@ -7,5 +8,6 @@ from .changelog import register_changelog
 def register_all_commands(bot, ctx):
     register_economy_commands(bot, ctx)
     register_economy_game_commands(bot, ctx)
+    register_economy_phase3_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
