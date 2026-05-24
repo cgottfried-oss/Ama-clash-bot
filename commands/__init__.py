07f7630b665931bf36cb901450a3c9a0708f14cd
cgottfried-oss <c.gottfried@icloud.com>
2026-05-24 02:46:37 -0700

    Register expanded economy game commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,9 +1,11 @@
 from .economy_commands import register_economy_commands
+from .economy_game_commands import register_economy_game_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
 
 
 def register_all_commands(bot, ctx):
     register_economy_commands(bot, ctx)
+    register_economy_game_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
