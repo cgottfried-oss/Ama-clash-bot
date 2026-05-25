7365da835a17f184f7eca1fe98900b440cfd9670
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 10:34:23 -0700

    Add Phase 5 core MMO infrastructure package

---
+++ features/phase5/core/__init__.py
@@ -0,0 +1,30 @@
+"""Shared MMO infrastructure for Phase 5 systems."""
+
+from .rarity import RARITIES, normalize_rarity, rarity_multiplier
+from .profiles import default_player_profile, ensure_player_profile
+from .inventory import default_inventory, make_item_stack, make_equipment_item
+from .cosmetics import default_cosmetics, unlock_cosmetic, equip_cosmetic
+from .matchmaking import default_matchmaking_profile, apply_match_result
+from .modifiers import StatBlock, Modifier, calculate_effective_stats
+from .events import EventBus, event_bus
+
+__all__ = [
+    "RARITIES",
+    "normalize_rarity",
+    "rarity_multiplier",
+    "default_player_profile",
+    "ensure_player_profile",
+    "default_inventory",
+    "make_item_stack",
+    "make_equipment_item",
+    "default_cosmetics",
+    "unlock_cosmetic",
+    "equip_cosmetic",
+    "default_matchmaking_profile",
+    "apply_match_result",
+    "StatBlock",
+    "Modifier",
+    "calculate_effective_stats",
+    "EventBus",
+    "event_bus",
+]
