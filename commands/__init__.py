39053152db330e890a9af61e603adba62d540edc
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 11:02:45 -0700

    Register Phase 5-2 cosmetic commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,6 +3,7 @@ from .economy_game_commands import register_economy_game_commands
 from .economy_phase3_commands import register_economy_phase3_commands
 from .economy_phase4_commands import register_economy_phase4_commands
 from .economy_phase5_1_commands import register_economy_phase5_1_commands
+from .economy_phase5_2_commands import register_economy_phase5_2_commands
 from .rpg_guide_commands import register_rpg_guide_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
@@ -14,6 +15,7 @@ def register_all_commands(bot, ctx):
     register_economy_phase3_commands(bot, ctx)
     register_economy_phase4_commands(bot, ctx)
     register_economy_phase5_1_commands(bot, ctx)
+    register_economy_phase5_2_commands(bot, ctx)
     register_rpg_guide_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
