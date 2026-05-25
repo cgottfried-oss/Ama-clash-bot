5776644d5d55ae14cb63327145a922cb6f8c8859
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 11:07:48 -0700

    Register Phase 5-3 equipment and hero commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,7 @@ from .economy_phase3_commands import register_economy_phase3_commands
 from .economy_phase4_commands import register_economy_phase4_commands
 from .economy_phase5_1_commands import register_economy_phase5_1_commands
 from .economy_phase5_2_commands import register_economy_phase5_2_commands
+from .economy_phase5_3_commands import register_economy_phase5_3_commands
 from .rpg_guide_commands import register_rpg_guide_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
@@ -16,6 +17,7 @@ def register_all_commands(bot, ctx):
     register_economy_phase4_commands(bot, ctx)
     register_economy_phase5_1_commands(bot, ctx)
     register_economy_phase5_2_commands(bot, ctx)
+    register_economy_phase5_3_commands(bot, ctx)
     register_rpg_guide_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
