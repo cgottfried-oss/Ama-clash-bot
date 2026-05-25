b21a8673cb56560d59aadf0ac98eb2808f3e1ee2
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 11:14:01 -0700

    Register Phase 5-4 matchmaking commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -5,6 +5,7 @@ from .economy_phase4_commands import register_economy_phase4_commands
 from .economy_phase5_1_commands import register_economy_phase5_1_commands
 from .economy_phase5_2_commands import register_economy_phase5_2_commands
 from .economy_phase5_3_commands import register_economy_phase5_3_commands
+from .economy_phase5_4_commands import register_economy_phase5_4_commands
 from .rpg_guide_commands import register_rpg_guide_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
@@ -18,6 +19,7 @@ def register_all_commands(bot, ctx):
     register_economy_phase5_1_commands(bot, ctx)
     register_economy_phase5_2_commands(bot, ctx)
     register_economy_phase5_3_commands(bot, ctx)
+    register_economy_phase5_4_commands(bot, ctx)
     register_rpg_guide_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
