f77bcab5808b7605616e17d76b0981272c4172b1
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 21:50:24 -0700

    Remove phase aliases from command registration

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,15 +1,15 @@
 from clash_mmo.commands.economy_commands import register_economy_commands
 from clash_mmo.commands.core_economy_commands import register_economy_game_commands
 from clash_mmo.commands.clan_economy_commands import register_clan_economy_commands
-from clash_mmo.commands.pvp_commands import register_economy_phase4_commands
-from clash_mmo.commands.season_commands import register_economy_phase5_1_commands
-from clash_mmo.commands.cosmetic_commands import register_economy_phase5_2_commands
-from clash_mmo.commands.gear_commands import register_economy_phase5_3_commands
-from clash_mmo.commands.ranked_commands import register_economy_commands as register_economy_phase5_4_commands
-from clash_mmo.commands.territory_commands import register_economy_phase5_5_commands
-from clash_mmo.commands.raid_commands import register_economy_phase5_6_commands
-from clash_mmo.commands.market_commands import register_economy_phase5_7_commands
-from clash_mmo.commands.event_commands import register_economy_phase5_8_commands
+from clash_mmo.commands.pvp_commands import register_economy_phase4_commands as register_pvp_commands
+from clash_mmo.commands.season_commands import register_economy_commands as register_season_commands
+from clash_mmo.commands.cosmetic_commands import register_economy_commands as register_cosmetic_commands
+from clash_mmo.commands.gear_commands import register_economy_commands as register_gear_commands
+from clash_mmo.commands.ranked_commands import register_economy_commands as register_ranked_commands
+from clash_mmo.commands.territory_commands import register_economy_phase5_5_commands as register_territory_commands
+from clash_mmo.commands.raid_commands import register_economy_phase5_6_commands as register_raid_commands
+from clash_mmo.commands.market_commands import register_economy_phase5_7_commands as register_market_commands
+from clash_mmo.commands.event_commands import register_economy_phase5_8_commands as register_event_commands
 from .rpg_guide_commands import register_rpg_guide_commands
 from .linking_commands import register_linking_commands
 from .changelog import register_changelog
@@ -19,15 +19,15 @@ def register_all_commands(bot, ctx):
     register_economy_commands(bot, ctx)
     register_economy_game_commands(bot, ctx)
     register_clan_economy_commands(bot, ctx)
-    register_economy_phase4_commands(bot, ctx)
-    register_economy_phase5_1_commands(bot, ctx)
-    register_economy_phase5_2_commands(bot, ctx)
-    register_economy_phase5_3_commands(bot, ctx)
-    register_economy_phase5_4_commands(bot, ctx)
-    register_economy_phase5_5_commands(bot, ctx)
-    register_economy_phase5_6_commands(bot, ctx)
-    register_economy_phase5_7_commands(bot, ctx)
-    register_economy_phase5_8_commands(bot, ctx)
+    register_pvp_commands(bot, ctx)
+    register_season_commands(bot, ctx)
+    register_cosmetic_commands(bot, ctx)
+    register_gear_commands(bot, ctx)
+    register_ranked_commands(bot, ctx)
+    register_territory_commands(bot, ctx)
+    register_raid_commands(bot, ctx)
+    register_market_commands(bot, ctx)
+    register_event_commands(bot, ctx)
     register_rpg_guide_commands(bot, ctx)
     register_linking_commands(bot, ctx)
     register_changelog(bot, ctx)
