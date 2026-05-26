5789af051de8f396a5da91c9fc2716ead15b6512
cgottfried-oss <c.gottfried@icloud.com>
2026-05-25 21:07:28 -0700

    Delete features/phase5 directory

--- features/phase5/marketplace/config.py
+++
@@ -1,5 +0,0 @@
-MARKETPLACE_CONFIG = {
-    "listing_tax": 0.05,
-    "auction_tax": 0.08,
-    "max_active_listings": 10,
-}
--- features/phase5/matchmaking/config.py
+++
@@ -1,6 +0,0 @@
-MATCHMAKING_CONFIG = {
-    "default_rating": 1000,
-    "search_range": 250,
-    "k_factor": 32,
-    "max_queue_size": 100,
-}
