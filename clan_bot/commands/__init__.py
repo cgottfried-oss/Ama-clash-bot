3aad74bc2ff21ec136a0172122dfbed17891b500
cgottfried-oss <c.gottfried@icloud.com>
2026-05-27 14:54:07 -0700

    Update __init__.py

--- clan_bot/commands/__init__.py
+++ clan_bot/commands/__init__.py
@@ -1,7 +1,5 @@
-from commands.linking_commands import register_linking_commands
-from commands.changelog import register_changelog
+from clan_bot.commands.linking_commands import register_linking_commands
 
 
 def register_clan_bot_commands(bot, ctx):
     register_linking_commands(bot, ctx)
-    register_changelog(bot, ctx)
