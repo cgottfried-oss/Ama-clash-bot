d9faf06aeb6f6faff2934ec6a0292d30b201a8dc
cgottfried-oss <c.gottfried@icloud.com>
2026-05-27 03:54:25 -0700

    Update __init__.py

--- clash_mmo/game/pve/__init__.py
+++ clash_mmo/game/pve/__init__.py
@@ -5,7 +5,6 @@ from .instances import create_raid_instance, get_active_raid, close_raid
 from .phases import get_boss_phase
 from .windows import is_damage_window_open, open_damage_window
 from .abilities import roll_boss_ability
-from .rewards import calculate_raid_rewards
 from .service import start_raid, join_raid, attack_raid_boss
 from .formatting import format_raid_status, format_attack_result
 
@@ -18,7 +17,6 @@ __all__ = [
     "is_damage_window_open",
     "open_damage_window",
     "roll_boss_ability",
-    "calculate_raid_rewards",
     "start_raid",
     "join_raid",
     "attack_raid_boss",
