eda11e12b5f31bc02d9bd676e83d2bf5db21134c
cgottfried-oss <c.gottfried@icloud.com>
2026-05-28 13:23:01 -0700

    Export gear upgrade helpers

--- clash_mmo/game/crafting/__init__.py
+++ clash_mmo/game/crafting/__init__.py
@@ -1,8 +1,14 @@
 from .salvage import SALVAGE_REWARDS, get_salvage_rewards
 from .service import salvage_item
+from .upgrade_service import upgrade_item
+from .upgrades import MAX_GEAR_UPGRADE_LEVEL, get_next_upgrade_cost, get_upgrade_level
 
 __all__ = [
     "SALVAGE_REWARDS",
     "get_salvage_rewards",
     "salvage_item",
+    "upgrade_item",
+    "MAX_GEAR_UPGRADE_LEVEL",
+    "get_next_upgrade_cost",
+    "get_upgrade_level",
 ]
