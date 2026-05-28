dde40b5133371f11fe0bb6f04fbbf35a175bd4c8
cgottfried-oss <c.gottfried@icloud.com>
2026-05-28 14:57:16 -0700

    Update __init__.py

--- clash_mmo/game/marketplace/__init__.py
+++ clash_mmo/game/marketplace/__init__.py
@@ -1,31 +1,43 @@
-"""player marketplace systems."""
+"""Player marketplace systems for the Clash MMO."""
 
 from .config import MARKETPLACE_CONFIG
-from .listings import create_listing, cancel_listing, get_active_listings
-from .auctions import create_auction, place_bid, close_auction
-from .trades import create_trade_offer, accept_trade_offer
 from .black_market import BLACK_MARKET_ITEMS, rotate_black_market
 from .pricing import calculate_tax, final_sale_amount
-from .service import buy_listing, list_inventory_item
 from .formatting import format_listing, format_auction, format_black_market
+from .auctions import create_auction, place_bid, close_auction
+from .economy import analyze_marketplace, filter_listings, item_market_traits
+from .service import (
+    get_active_listings,
+    create_market_listing,
+    buy_market_listing,
+    cancel_market_listing,
+    create_trade_offer,
+    accept_trade_offer,
+    decline_trade_offer,
+    expire_marketplace_entries,
+)
 
 __all__ = [
     "MARKETPLACE_CONFIG",
-    "create_listing",
-    "cancel_listing",
-    "get_active_listings",
-    "create_auction",
-    "place_bid",
-    "close_auction",
-    "create_trade_offer",
-    "accept_trade_offer",
     "BLACK_MARKET_ITEMS",
     "rotate_black_market",
     "calculate_tax",
     "final_sale_amount",
-    "buy_listing",
-    "list_inventory_item",
     "format_listing",
     "format_auction",
     "format_black_market",
+    "create_auction",
+    "place_bid",
+    "close_auction",
+    "analyze_marketplace",
+    "filter_listings",
+    "item_market_traits",
+    "get_active_listings",
+    "create_market_listing",
+    "buy_market_listing",
+    "cancel_market_listing",
+    "create_trade_offer",
+    "accept_trade_offer",
+    "decline_trade_offer",
+    "expire_marketplace_entries",
 ]
