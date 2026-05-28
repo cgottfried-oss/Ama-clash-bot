b8b89f4d063cb7c84341108689a804f2468e2f0a
cgottfried-oss <c.gottfried@icloud.com>
2026-05-28 13:07:22 -0700

    Add Clash-themed crafting package

---
+++ clash_mmo/game/crafting/__init__.py
@@ -0,0 +1,8 @@
+from .salvage import SALVAGE_REWARDS, get_salvage_rewards
+from .service import salvage_item
+
+__all__ = [
+    "SALVAGE_REWARDS",
+    "get_salvage_rewards",
+    "salvage_item",
+]
