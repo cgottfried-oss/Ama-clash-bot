1dd167ef86525556e239ef85ad206cb5dff8e9ab
cgottfried-oss <c.gottfried@icloud.com>
2026-05-28 16:54:41 -0700

    Update economy_commands.py

--- clash_mmo/commands/economy_commands.py
+++ clash_mmo/commands/economy_commands.py
@@ -3,11 +3,9 @@ from __future__ import annotations
 from clash_mmo.commands.wallet_commands import register_wallet_commands
 from clash_mmo.commands.shop_commands import register_shop_commands
 from clash_mmo.commands.loot_commands import register_loot_commands
-from clash_mmo.commands.steal_commands import register_steal_commands
 
 
 def register_economy_commands(bot, ctx):
     register_wallet_commands(bot, ctx)
     register_shop_commands(bot, ctx)
     register_loot_commands(bot, ctx)
-    register_steal_commands(bot, ctx)
