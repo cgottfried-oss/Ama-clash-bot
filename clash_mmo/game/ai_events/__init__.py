380934b936d5f129e7a4b13b06d82227fe6809ab
cgottfried-oss <c.gottfried@icloud.com>
2026-05-30 09:37:43 -0700

    patch68: update clash_mmo/game/ai_events/__init__.py

--- clash_mmo/game/ai_events/__init__.py
+++ clash_mmo/game/ai_events/__init__.py
@@ -1,24 +1,12 @@
 from __future__ import annotations
 
-from clash_mmo.game.ai_events.effects import apply_event_effects
-from clash_mmo.game.ai_events.formatting import format_event_embed, format_event_summary
-from clash_mmo.game.ai_events.generator import generate_ai_event
-from clash_mmo.game.ai_events.modifiers import get_active_event_modifiers
-from clash_mmo.game.ai_events.scheduler import schedule_next_event
-from clash_mmo.game.ai_events.service import ensure_event_state, get_active_events, resolve_event
-from clash_mmo.game.ai_events.targets import get_event_target_pool
-from clash_mmo.game.ai_events.templates import EVENT_TEMPLATES
+from .formatting import format_event_card, format_event_list
+from .service import create_ai_event, get_active_events, resolve_ai_event
 
 __all__ = [
-    "EVENT_TEMPLATES",
-    "apply_event_effects",
-    "ensure_event_state",
-    "format_event_embed",
-    "format_event_summary",
-    "generate_ai_event",
-    "get_active_event_modifiers",
+    "create_ai_event",
+    "format_event_card",
+    "format_event_list",
     "get_active_events",
-    "get_event_target_pool",
-    "resolve_event",
-    "schedule_next_event",
+    "resolve_ai_event",
 ]
