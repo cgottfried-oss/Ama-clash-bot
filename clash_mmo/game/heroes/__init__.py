5a963536b4dc4a5ef06c22ba6086ca496508346f
cgottfried-oss <c.gottfried@icloud.com>
2026-05-30 09:40:04 -0700

    patch68: update clash_mmo/game/heroes/__init__.py

--- clash_mmo/game/heroes/__init__.py
+++ clash_mmo/game/heroes/__init__.py
@@ -1,37 +1,46 @@
 from clash_mmo.game.heroes.catalog import (
+    ENABLED_HERO_IDS,
     HERO_CATALOG,
-    HERO_ORDER,
-    HERO_UNLOCKS,
     enabled_hero_ids,
-    hero_display_name,
+    get_hero_config,
+    get_hero_name,
+    get_hero_unlock_th,
+    is_enabled_hero,
     unlocked_hero_ids_for_town_hall,
 )
-from clash_mmo.game.heroes.loadouts import normalize_hero_loadouts
-from clash_mmo.game.heroes.progression import (
-    get_hero_upgrade_cost,
-    get_hero_xp_needed,
+from clash_mmo.game.heroes.loadouts import (
+    get_active_hero_id,
+    get_profile_hero_level,
+    hero_is_unlocked,
+    normalize_hero_loadouts,
+    set_active_hero,
+    unlock_hero,
+)
+from clash_mmo.game.heroes.progression import MAX_HERO_LEVEL, get_hero_upgrade_cost, get_max_hero_level
+from clash_mmo.game.heroes.service import (
+    ensure_unlocked_heroes_for_town_hall,
     get_total_hero_power,
 )
-from clash_mmo.game.heroes.service import (
-    add_hero_xp,
-    can_upgrade_hero,
-    unlock_hero,
-    upgrade_hero,
-)
+
 
 __all__ = [
+    "ENABLED_HERO_IDS",
     "HERO_CATALOG",
-    "HERO_ORDER",
-    "HERO_UNLOCKS",
     "enabled_hero_ids",
-    "hero_display_name",
+    "get_hero_config",
+    "get_hero_name",
+    "get_hero_unlock_th",
+    "is_enabled_hero",
     "unlocked_hero_ids_for_town_hall",
+    "get_active_hero_id",
+    "get_profile_hero_level",
+    "hero_is_unlocked",
     "normalize_hero_loadouts",
-    "get_hero_upgrade_cost",
-    "get_hero_xp_needed",
-    "get_total_hero_power",
-    "add_hero_xp",
-    "can_upgrade_hero",
+    "set_active_hero",
     "unlock_hero",
-    "upgrade_hero",
+    "MAX_HERO_LEVEL",
+    "get_max_hero_level",
+    "get_hero_upgrade_cost",
+    "ensure_unlocked_heroes_for_town_hall",
+    "get_total_hero_power",
 ]
