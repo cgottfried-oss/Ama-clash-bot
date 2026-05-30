ff9efb1a8eb5bedd2dd18eeafd14932d64470b53
cgottfried-oss <c.gottfried@icloud.com>
2026-05-30 09:39:32 -0700

    patch68: update clash_mmo/game/pve/__init__.py

--- clash_mmo/game/pve/__init__.py
+++ clash_mmo/game/pve/__init__.py
@@ -1,1 +1,25 @@
 """cooperative real-time PvE systems."""
+
+from .bosses import RAID_BOSSES
+from .instances import create_raid_instance, get_active_raid, close_raid
+from .phases import get_boss_phase
+from .windows import is_damage_window_open, open_damage_window
+from .abilities import roll_boss_ability
+from .service import start_raid, join_raid, attack_raid_boss
+from .formatting import format_raid_status, format_attack_result
+
+__all__ = [
+    "RAID_BOSSES",
+    "create_raid_instance",
+    "get_active_raid",
+    "close_raid",
+    "get_boss_phase",
+    "is_damage_window_open",
+    "open_damage_window",
+    "roll_boss_ability",
+    "start_raid",
+    "join_raid",
+    "attack_raid_boss",
+    "format_raid_status",
+    "format_attack_result",
+]
