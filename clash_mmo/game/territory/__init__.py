52d745dbfa94e771134e68f985127fd2e145ac44
cgottfried-oss <c.gottfried@icloud.com>
2026-05-30 18:59:18 -0700

    patch68: update clash_mmo/game/territory/__init__.py

--- clash_mmo/game/territory/__init__.py
+++ clash_mmo/game/territory/__init__.py
@@ -4,6 +4,7 @@ from .regions import TERRITORY_REGIONS
 from .ownership import claim_region, get_region_owner, release_region
 from .resources import calculate_region_income, collect_region_income
 from .conquest import resolve_conquest
+from .npc_raids import run_npc_territory_raids
 from .season import current_territory_season, add_conquest_points
 from .formatting import format_region_line, format_territory_map
 
@@ -15,6 +16,7 @@ __all__ = [
     "calculate_region_income",
     "collect_region_income",
     "resolve_conquest",
+    "run_npc_territory_raids",
     "current_territory_season",
     "add_conquest_points",
     "format_region_line",
