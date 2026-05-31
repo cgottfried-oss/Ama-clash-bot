b4432c6dd0a0cab9547e4b73a4ebc22bf4db136b
cgottfried-oss <c.gottfried@icloud.com>
2026-05-31 10:31:55 -0700

    patch68: update bot_runner.py

--- bot_runner.py
+++ bot_runner.py
@@ -53,6 +53,7 @@ from clan_bot.snapshot_progress.progress_commands import register_current_progre
 from clash_mmo.services.economy import EconomyManager
 import clash_mmo.services.loot_drops as loot_drops
 from clash_mmo.config.economy_config import SHOP_ITEMS, LOOT_DROP_STYLES
+from clash_mmo.game.state import update_mmo_state, load_mmo_state
 
 # Load .env
 load_dotenv()
